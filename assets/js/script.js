b19e63e06aaa32fa21c1916b33a9c3cfd5847668
Fabi <58353647+FabiBrachetta@users.noreply.github.com>
2020-09-03 22:39:38 +0200

    Add dataset, logic to game, shuffle and usability

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,61 @@
 const cards = document.querySelectorAll('.memory-card');
 
-function flipCard() {
-  this.classList.toggle('flip');
-}
+  let hasFlippedCard = false;
+  let lockBoard = false;
+  let firstCard, secondCard;
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+  function flipCard() {
+    if (lockBoard) return;
+   if (this === firstCard) return;
+
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+      hasFlippedCard = true;
+      firstCard = this;
+      return;
+    }
+
+    secondCard = this;
+   hasFlippedCard = false;
+
+    checkForMatch();
+  }
+
+  function checkForMatch() {
+    let isMatch = firstCard.dataset.image === secondCard.dataset.image;
+    isMatch ? disableCards() : unflipCards();
+  }
+
+  function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+   resetBoard();
+  }
+
+  function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+
+     lockBoard = false;
+     resetBoard();
+    }, 1500);
+  }
+
+  function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+  }
+
+ (function shuffle() {
+   cards.forEach(card => {
+     let ramdomPos = Math.floor(Math.random() * 16);
+     card.style.order = ramdomPos;
+   });
+ })();
+
+  cards.forEach(card => card.addEventListener('click', flipCard));

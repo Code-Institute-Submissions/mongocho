7a5db54af104c4033c8e991e96cb050db929b962
Fabi <58353647+FabiBrachetta@users.noreply.github.com>
2020-09-17 20:38:55 +0200

    Comment code in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
   const cards = document.querySelectorAll(".memory-card");
   const modal = document.querySelector(".stat-modal");
-  const closeButton = document.getElementById("close");
-  const replayButton = document.getElementById("play");
-  const counterDisplay = document.getElementById("counter-1");
-  const restartButton = document.querySelector(".restart-btn");
+  const closeButton = document.getElementById("close"); // closes the modal after ending the game
+  const replayButton = document.getElementById("play"); // replay button on the modal after ending the game
+  const counterDisplay = document.getElementById("counter-1"); // displays the number of pair counts on the board
+  const restartButton = document.querySelector(".restart-btn"); // restarts the game
 
   let hasFlippedCard = false;
   let lockBoard = false;
   let firstCard, secondCard;
-  let counter = 0;
-  let closedCards = 0;
+  let counter = 0; // stores the count of pairs you have opened
+  let closedCards = 0;  // this variable determines when the game is finished, if closedCards equal 0 then the game has ended
 
   function flipCard() {
     if (lockBoard) return;
@@ -27,7 +27,7 @@ $(document).ready(function () {
     secondCard = this;
     hasFlippedCard = false;
     counter++;
-    counterDisplay.innerHTML = `Total pairs flipped: ${counter}`;
+    counterDisplay.innerHTML = `Total pairs flipped: ${counter}`; // increment the counter variable everytime a pair is opened
 
     checkForMatch();
   }
@@ -40,15 +40,16 @@ $(document).ready(function () {
   function disableCards() {
     firstCard.removeEventListener("click", flipCard);
     secondCard.removeEventListener("click", flipCard);
-    closedCards -= 2;
+    closedCards -= 2; // initially closedCards = 16, when successfully find a match value reduces by 2
+    
     if (closedCards == 0) {
       setTimeout(() => {
         document.getElementById(
           "counter-2"
-        ).innerHTML = `Total pairs flipped: ${counter}`;
-        modal.style.display = "flex";
-      }, 850);
-    }
+        ).innerHTML = `Total pairs flipped: ${counter}`; // runs to display the modal when closedCards = 0 (i.e when all matchs have being found)
+        modal.style.display = "flex"; 
+      }, 850); 
+    }// makes the modal visible as it's hidden by default
     resetBoard();
   }
 
@@ -71,7 +72,7 @@ $(document).ready(function () {
 
   const shuffle = () => {
     cards.forEach((card) => {
-      let ramdomPos = Math.floor(Math.random() * 16);
+      let ramdomPos = Math.floor(Math.random() * 16); // places cards ramdonly on restart
       card.style.order = ramdomPos;
       closedCards++;
       card.addEventListener("click", flipCard);

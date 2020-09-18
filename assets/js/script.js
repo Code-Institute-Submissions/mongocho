57f305fd0ca15d780443c2ef2383bef01155c512
Fabi <58353647+FabiBrachetta@users.noreply.github.com>
2020-09-18 14:50:04 +0200

    Add comments to JavaScript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,22 @@
 $(document).ready(function () {
   const cards = document.querySelectorAll(".memory-card");
   const modal = document.querySelector(".stat-modal");
-  const closeButton = document.getElementById("close"); // closes the modal after ending the game
-  const replayButton = document.getElementById("play"); // replay button on the modal after ending the game
-  const counterDisplay = document.getElementById("counter-1"); // displays the number of pair counts on the board
-  const restartButton = document.querySelector(".restart-btn"); // restarts the game
+  // closes the modal after ending the game
+  const closeButton = document.getElementById("close");
+  // replay button on the modal after ending the game
+  const replayButton = document.getElementById("play");
+  // displays the number of pair counts on the board
+  const counterDisplay = document.getElementById("counter-1");
+  // restarts the game
+  const restartButton = document.querySelector(".restart-btn");
 
   let hasFlippedCard = false;
   let lockBoard = false;
   let firstCard, secondCard;
-  let counter = 0; // stores the count of pairs you have opened
-  let closedCards = 0;  // this variable determines when the game is finished, if closedCards equal 0 then the game has ended
+  // stores the count of pairs you have opened
+  let counter = 0;
+  // this variable determines when the game is finished, if closedCards equal 0 then the game has ended
+  let closedCards = 0;
 
   function flipCard() {
     if (lockBoard) return;
@@ -24,10 +30,11 @@ $(document).ready(function () {
       return;
     }
 
+    // increment the counter variable everytime a pair is opened
     secondCard = this;
     hasFlippedCard = false;
     counter++;
-    counterDisplay.innerHTML = `Total pairs flipped: ${counter}`; // increment the counter variable everytime a pair is opened
+    counterDisplay.innerHTML = `Total pairs flipped: ${counter}`;
 
     checkForMatch();
   }
@@ -37,19 +44,21 @@ $(document).ready(function () {
     isMatch ? disableCards() : unflipCards();
   }
 
+  // initially closedCards = 16, when successfully find a match value reduces by 2
   function disableCards() {
     firstCard.removeEventListener("click", flipCard);
     secondCard.removeEventListener("click", flipCard);
-    closedCards -= 2; // initially closedCards = 16, when successfully find a match value reduces by 2
-    
+    closedCards -= 2;
+
+    // makes the modal visible as it's hidden by default
     if (closedCards == 0) {
       setTimeout(() => {
         document.getElementById(
           "counter-2"
         ).innerHTML = `Total pairs flipped: ${counter}`; // runs to display the modal when closedCards = 0 (i.e when all matchs have being found)
-        modal.style.display = "flex"; 
-      }, 850); 
-    }// makes the modal visible as it's hidden by default
+        modal.style.display = "flex";
+      }, 850);
+    }
     resetBoard();
   }
 
@@ -70,15 +79,19 @@ $(document).ready(function () {
     [firstCard, secondCard] = [null, null];
   }
 
+  // places cards ramdonly on restart
   const shuffle = () => {
     cards.forEach((card) => {
-      let ramdomPos = Math.floor(Math.random() * 16); // places cards ramdonly on restart
+      let ramdomPos = Math.floor(Math.random() * 16);
       card.style.order = ramdomPos;
       closedCards++;
       card.addEventListener("click", flipCard);
     });
   };
 
+  // this function is what restarts the game when the restart button or replay button is clicked.
+  // It hides the modal, return counter and closedCards to zero and remove the .flip css class
+  // so the cards can be flipped back to there hidden state
   const restart = () => {
     modal.style.display = "none";
     closedCards = 0;
@@ -100,14 +113,17 @@ $(document).ready(function () {
     });
   }
 
+  // event listener added to the replay button to restart the game from modal
   if (replayButton) {
     replayButton.addEventListener("click", restart);
   }
 
+  // event listener added to the restart button to restart the game from board
   if (replayButton) {
     restartButton.addEventListener("click", restart);
   }
 
+  //shuffle the card
   shuffle();
 });
 
